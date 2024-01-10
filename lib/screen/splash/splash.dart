9ed5b3861bf9b73f258fccfa7997c263bf7e1b8d
saadbhati1002 <71560001+saadbhati1002@users.noreply.github.com>
2024-01-10 16:48:33 +0530

    feat: splash screen design

--- lib/screen/splash/splash.dart
+++ lib/screen/splash/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tourist/utility/images.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,7 +10,20 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          height: 100,
+          width: MediaQuery.of(context).size.width * .6,
+          child: Image.asset(Images.logo),
+        ),
+      ),
+    );
   }
 }
