352026b952509f18a785d155bba7d279b8d7c0cc
saadbhati1002 <71560001+saadbhati1002@users.noreply.github.com>
2024-01-14 23:29:59 +0530

    feat: splash screen navigation if login

--- lib/screen/splash/splash.dart
+++ lib/screen/splash/splash.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:tourist/models/user/user_model.dart';
 import 'package:tourist/screen/auth/login/login_screen.dart';
+import 'package:tourist/screen/dashboard/dashboard_screen.dart';
+import 'package:tourist/utility/constant.dart';
 import 'package:tourist/utility/images.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -14,9 +19,23 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     Future.delayed(const Duration(seconds: 2), () {
-      Get.to(() => const LoginScreen());
+      checkLogin();
+      // Get.to(() => const LoginScreen());
     });
     super.initState();
+  }
+
+  checkLogin() async {
+    try {
+      var response = await AppConstant.getUserDetail();
+      if (response != null && response != "null") {
+        UserRes responseUser = UserRes.fromJson(jsonDecode(response));
+        AppConstant.userData = responseUser.success;
+        Get.to(() => const DashBoardScreen());
+      }
+    } catch (e) {
+      Get.to(() => const LoginScreen());
+    }
   }
 
   @override
