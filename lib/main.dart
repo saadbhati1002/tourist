55655e4f1b2dc690513567adbd045f5a5b08e5d0
saadbhati1002 <saadbhati1002@gmail.com>
2024-02-01 19:30:22 +0530

    feat: ios firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,32 @@
 import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:tourist/screen/splash/splash.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:tourist/utility/constant.dart';
+import 'firebase_options.dart';
 
-void main() async {
-  HttpOverrides.global = MyHttpOverrides();
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: 'AIzaSyCym2QnkvpdA4LQ-wPQdIzDNUF2AIx7B2c',
+      appId: '1:104570725171:android:daf15260471a4f2c40d446',
+      messagingSenderId: '104570725171',
+      projectId: 'dubai-tourist',
+      storageBucket: 'dubai-tourist.appspot.com',
+      androidClientId:
+          '475549178638-4n2hgqgn6mkgnjma33kv8lceb7s8g3vo.apps.googleusercontent.com',
+      iosClientId:
+          '104570725171-mj077noqeq8fer2ahph5du3gn4nvblkt.apps.googleusercontent.com',
+      iosBundleId: 'com.tourist.vivah',
+    ),
+  );
+  HttpOverrides.global = MyHttpOverrides();
+
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
